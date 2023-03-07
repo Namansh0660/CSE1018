ad0f0892342efd820d830f04c78000939ca006ad
Namansh0660 <115533264+Namansh0660@users.noreply.github.com>
2023-03-07 11:25:56 +0530

    Add files via upload

---
+++ ClassDemo.java
@@ -0,0 +1,13 @@
+class Box{
+    int length;
+    int breadth;
+    int height;
+}
+public class ClassDemo{
+    public static void main(String args[]){
+        Box blackbox =new Box();
+        blackbox.length=10;
+        blackbox.breadth=8;
+        System.out.println(blackbox.length);
+    }
+}
