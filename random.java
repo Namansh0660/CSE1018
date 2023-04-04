65848acded26bd7112c326d89cc766c0784c25de
Namansh <115533264+Namansh0660@users.noreply.github.com>
2023-04-04 10:50:00 +0530

    Add files via upload

---
+++ random.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+public class random
+{
+public static void main(String args[])
+{
+double x =Math.random();
+double y =Math.random();
+System.out.println("X : "+x);
+System.out.println("Y : "+y);
+System.out.println("X+Y : "+(x+y));
+}
+}
---
+++ random1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class random1
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+double n=sc.nextDouble();
+n=Math.random()*20;
+System.out.println("Math Random : "+n);
+}
+}
