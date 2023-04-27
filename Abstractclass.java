1a53a1357a8abd88002333a7e2f997a169091ea8
Namansh <115533264+Namansh0660@users.noreply.github.com>
2023-04-27 10:15:01 +0530

    Create Abstractclass.java

---
+++ Abstractclass.java
@@ -0,0 +1,31 @@
+abstract class Member{
+    private String name;
+    abstract void welcomeMessage();
+}
+class Student extends Member{
+    void welcomeMessage(){
+        System.out.println("Hello Student");
+    }
+}
+class Teacher extends Member{
+    void welcomeMessage(){
+        System.out.println("Hello Teacher");
+    }
+}
+class general{
+    void check(Member v)
+    {
+        System.out.println("checking");
+        v.welcomeMessage();
+    }
+}
+public class AbstractcollageDemo{
+    public static void main(String args[])
+    {
+        general m1=new general();
+        Student s1=new Student();
+        Teacher t1=new Teacher();
+        m1.check(s1);
+        m1.check(t1);
+    }
+}
