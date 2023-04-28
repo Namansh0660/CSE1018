0aaf4df32a88eff047234221eeba49b670b9e9e7
Namansh <115533264+Namansh0660@users.noreply.github.com>
2023-04-28 09:32:46 +0530

    Update Package1.java

--- Package1.java
+++ Package1.java
@@ -9,7 +9,6 @@ public class A{
 //in 2nd class
 package mypack;  
 import pack.A;  
-  
 class B{  
   public static void main(String args[]){  
    A obj = new A();  
@@ -20,3 +19,22 @@ class B{
 //javac -d . A.java
 //javac -d . B.java
 //java mypack.B
+
+
+//save by C.java
+//in 3rd java
+package mypack;
+import pack.A;
+class C{
+    public static void main(String args[])
+    {
+        A obj = new A();
+        obj.msg();
+    }
+}
+
+
+
+//javac -d . A.java
+//javac -d . C.java
+//java mypack.C
