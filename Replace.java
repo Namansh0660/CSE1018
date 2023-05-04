f72bd1ce8ac8bf88ace41441dba183c4d7eb5ec8
Namansh <115533264+Namansh0660@users.noreply.github.com>
2023-05-04 11:11:54 +0530

    Replace.java

---
+++ Replace.java
@@ -0,0 +1,18 @@
+class Replace{
+    public static void main(String args[]){
+        String mystr="Hello";
+        System.out.println(mystr.replace('l','p'));
+        String s="  Hello world     ";
+        System.out.println(s);
+        System.out.println(s.trim());
+        String txt1="chennai";
+        String txt="HELLO WORLD";
+        System.out.println(txt1.toUpperCase());
+        System.out.println(txt.toLowerCase());
+        String s2="Java";
+        String s3="";
+        System.out.println(s2.isEmpty());
+        System.out.println(s3.isEmpty());
+        
+    }
+}
