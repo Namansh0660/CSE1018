3455d1f9353b951f6c5412b3ddc33bb830ebcf5d
Namansh <115533264+Namansh0660@users.noreply.github.com>
2023-05-16 10:32:25 +0530

    Create Strings.java

---
+++ Strings.java
@@ -0,0 +1,39 @@
+class Strings {
+    public static void main(String[] args) 
+    {
+        StringBuffer sb =new StringBuffer("abcdefghi");
+        System.out.println(sb);
+    }
+}
+class HelloWorld {
+    public static void main(String[] args) 
+    {
+        StringBuilder sb =new StringBuilder("abcdefghi");
+        sb.reverse();
+        System.out.println(sb);
+    }
+}
+class HelloWorld {
+    public static void main(String[] args) 
+    {
+        StringBuffer sb =new StringBuffer("abcdefghi");
+        sb.delete(3,7);
+        System.out.println(sb);
+    }
+}
+class HelloWorld {
+    public static void main(String[] args) 
+    {
+        StringBuffer sb =new StringBuffer("abcdefghi");
+        sb.replace(3,7);
+        System.out.println(sb);
+    }
+}
+class HelloWorld {
+    public static void main(String[] args) 
+    {
+        StringBuffer sb =new StringBuffer("abcdefghi");
+        sb.insert(3,7);
+        System.out.println(sb);
+    }
+}
