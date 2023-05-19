c789715aa6fd34aa2b8704039f38f8eb693ae7a5
Namansh <115533264+Namansh0660@users.noreply.github.com>
2023-05-19 09:22:51 +0530

    Threading5.java

---
+++ Threading5.java
@@ -0,0 +1,29 @@
+class Example extends Thread
+{
+    public void run()
+    {
+        Thread.yield();
+        for (int i=0;i<5;i++)
+        {
+            System.out.println("Child thread" + i);
+        }
+    }
+}
+class Main
+{
+    public static void main(String[] args)
+    {
+        Example obj=new Example();
+        System.out.println(obj.getState());
+        obj.setPriority(1);
+        System.out.println(obj.getName());
+        obj.setName("Customer");
+        System.out.println(obj.getName());
+        System.out.println(obj.getId());
+        for (int i=0;i<5;i++)
+        {
+            System.out.println("main thread" + i);
+        }
+        System.out.println(obj.getState());
+    }
+}
