5095875a69d8dd3c7c5a9b180edb2e1320c896de
Namansh <115533264+Namansh0660@users.noreply.github.com>
2023-05-19 09:14:47 +0530

    Threading3.java

---
+++ Threading3.java
@@ -0,0 +1,25 @@
+class Example extends Thread
+{
+    public void run()
+    {
+        for (int i=0;i<5;i++)
+        {
+            System.out.println("Child thread" + i);
+        }
+    }
+}
+class Main
+{
+    public static void main(String[] args)
+    {
+        Example obj=new Example();
+        System.out.println(obj.getState());
+        obj.setPriority(1);
+        System.out.println(obj.getName());
+        for (int i=0;i<5;i++)
+        {
+            System.out.println("main thread" + i);
+        }
+        System.out.println(obj.getState());
+    }
+}
